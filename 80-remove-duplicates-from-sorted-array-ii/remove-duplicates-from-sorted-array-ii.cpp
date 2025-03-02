618aa18e3573fa1059c8858ac5f02ebfd6a1168b
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-01 23:59:57 -0500

    Update remove-duplicates-from-sorted-array-ii.cpp

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -16,3 +16,33 @@ public:
         
     }
 };
+
+
+
+/*
+USING COUNT METHOD:
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count=1;
+        int k=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]){
+                count++;
+            }
+            else{
+                count=1;
+            }
+            if(count<=2){
+                nums[k]=nums[i];
+                k++;
+                }
+        
+        
+        }
+        return k;
+    }
+};
+
+*/
