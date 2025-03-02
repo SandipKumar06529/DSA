bca30535c25a380f4d4e6b90a73b0a82d1105e0f
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-02 01:36:39 -0500

    Update rotate-array.cpp

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -15,3 +15,31 @@ public:
         nums=temp;
     }
 };
+
+
+/*
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n =nums.size();
+       vector<int> temp(n);
+        for (int i=0;i<n;i++){
+            temp[(i+k)%n] =nums[i];
+        }
+       nums=temp;
+    }
+};
+
+
+explainatin
+
+Original Index (i)	Element (nums[i])	New Index ((i + k) % n)
+0	        1	        (0 + 3) % 7     = 3
+1	        2	        (1 + 3) % 7     = 4
+2	        3	        (2 + 3) % 7     = 5
+3	        4	        (3 + 3) % 7     = 6
+4	        5	        (4 + 3) % 7     = 0
+5	        6	        (5 + 3) % 7     = 1
+6	        7	        (6 + 3) % 7     = 2
+
+*/
