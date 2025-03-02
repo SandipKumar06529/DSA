ffbf82036c282e5a35dde209c8239e3a935ec1fc
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-01 23:40:46 -0500

    Update remove-duplicates-from-sorted-array.cpp

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -13,3 +13,26 @@ public:
         
     }
 };
+
+
+
+// using hashmap
+/*
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_map<int, bool> hashmap; // HashMap to track unique numbers
+        int index = 0; // Pointer to insert unique elements
+        
+        for (int num : nums) { // Iterate through the array
+            if (hashmap.find(num) == hashmap.end()) { // If num is NOT in hashmap
+                hashmap[num] = true; // Mark num as found
+                nums[index] = num; // Store unique num in-place
+                index++; // Move to next position
+            }
+        }
+        
+        return index; // New length of unique elements
+    }
+};
+*/
