ab1712870f1ecd81a7be390cc9c4260e08eb2ad2
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-03 00:28:15 -0500

    Update majority-element.cpp

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,12 +5,12 @@ public:
         unordered_map<int,int>m;
 
         for(int i=0; i<x; i++){
-            m[nums[i]]++;
+            m[nums[i]]++; // increase the count 3->1, 2->1,3->2 
         }
         x = x/2;
         for(auto y:m){
-            if(y.second > x){
-                return y.first;
+            if(y.second > x){  // y.second is the element= count>x/2= >3->2
+                return y.first;  // y.first is key => num
             }
         }
         return 0;
