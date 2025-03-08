3544b5a166069d070af055b791589db33bbef987
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-08 13:17:29 -0500

    Update length-of-last-word.cpp

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -4,13 +4,22 @@ public:
         int n =s.length();
         int length=0;
         int i = n-1;
-        while(i>=0 && s[i]==' '){
+        while(i>=0 && s[i]==' '){ // skip the spaces
             i--;
         }
-        while(i>=0 && s[i]!=' '){
-            length++;
+        while(i>=0 && s[i]!=' '){ // count the letter
+            length++;       // update the lendth till the next space.
             i--;
         }
+
+        /* Using for loop
+        int length=0;
+        for(int i=s.size()-1;i>=0;--i){
+            if(s[i]==' '&&length>0) break;
+            if(s[i]!=' ') ++length;
+        }
+        
+        */
         return length;
     }
 };
