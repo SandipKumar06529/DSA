297a3f2279fda98c581d6e2f914020b87fc9f3e0
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-09 23:52:20 -0400

    Time: 0 ms (100.00%) | Memory: 13.4 MB (19.83%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> romanValues = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
-            {'C', 100}, {'D', 500}, {'M', 1000}
-        };
-
-        int total = 0;
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
-            // Check if the next numeral is larger (subtractive case)
-            if (i + 1 < n && romanValues[s[i]] < romanValues[s[i + 1]]) {
-                total -= romanValues[s[i]];  // Subtract value
+        unordered_map<char, int> romanMap = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+        
+        int result = 0;
+        int prevValue = 0;
+        
+        for (int i = s.size() - 1; i >= 0; i--) {
+            int currentValue = romanMap[s[i]];
+            if (currentValue < prevValue) {
+                result = result - currentValue;
             } else {
-                total += romanValues[s[i]];  // Add value
+                result = result + currentValue;
             }
+            
+            prevValue = currentValue;
         }
-
-        return total;
+        
+        return result;
         
     }
 };
