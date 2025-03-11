4f2ca09c450abb7cf34c15555967df7894c03062
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-10 23:44:04 -0400

    Update find-the-index-of-the-first-occurrence-in-a-string.cpp

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -10,3 +10,21 @@ public:
       
     }
 };
+
+
+
+/*
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {  
+        int pos = haystack.find(needle); 
+        if (haystack.find(needle) != string::npos) {
+            return pos; 
+        } else {
+            return -1; 
+        }
+      
+    }
+}
+*/
