cd2e0cd010a13f01c81130b41f7a9521c6a61366
Sandipkumar <158214102+SandipKumar06529@users.noreply.github.com>
2025-03-28 20:17:47 -0400

    Time: 2 ms (56.02%) | Memory: 7.8 MB (69.60%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -5,10 +5,10 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int first=1, last=n;
-        while(first<last){
+        while(first<=last){
             int mid = (last-first)/2 + first;
             if(isBadVersion(mid)==true){ //the first bad version could be mid or left half, so update last 
-                last=mid; //version higher than mid are all bad.
+                last=mid-1; //version higher than mid are all bad.
             }
             else{// the first bad version must be in the right half, so update first 
                 first=mid+1; // version lower than mdi are all good.
